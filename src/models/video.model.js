6af77014b586874a3aafc57924bc556013297152
Nikhil Prasad <nikhilprasadinbox@gmail.com>
2024-03-24 23:13:22 +0530

    creating models

---
+++ src/models/video.model.js
@@ -0,0 +1,39 @@
+import mongoose, { Schema } from "moongose";
+const videoSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    videoFile: {
+      type: String, //cloudinary
+      required: true,
+    },
+    thumbnail: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+      required: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const video = mongoose.model("video", videoSchema);
