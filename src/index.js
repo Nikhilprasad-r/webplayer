a59ea9067d149414c21636df5e7f05b1122f08fd
Nikhil Prasad <nikhilprasadinbox@gmail.com>
2024-03-24 18:27:11 +0530

    created utility function to async

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import connectDB from "./db/index.js";
-
-connectDB();
+import { app } from "./app.js";
+connectDB()
+  .then(() => {
+    app.get("/api/v1/id", (req, res) => {
+      res.json("Hello World");
+    });
+    app.listen(process.env.PORT || 3000, () => {
+      console.log("Server running");
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+  });
