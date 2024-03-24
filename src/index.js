f57a9c9aa0966e7cce022eda4f39317410a31f0f
Nikhil Prasad <nikhilprasadinbox@gmail.com>
2024-03-24 12:14:18 +0530

    db connected

--- src/index.js
+++ src/index.js
@@ -1,19 +1,3 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants.js";
-import express from "express";
-const app = express();
+import connectDB from "./db/index.js";
 
-(async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    app.on("error", (err) => {
-      console.error(err);
-      throw err;
-    });
-    app.listen(process.env.PORT || 3000, () => {
-      console.log("Server running on port 3000");
-    });
-  } catch (e) {
-    console.error(e);
-  }
-})();
+connectDB();
